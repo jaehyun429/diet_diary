7cf9c5a4cccca30a32e9c3ad71d46dee6bc73feb
kyungseopark <separk823@gmail.com>
2024-08-19 02:09:15 +0900

    kyung2

--- login.js
+++ login.js
@@ -14,7 +14,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     // 서버와의 통신을 위한 가상 코드 (예: API 호출)
     // 실제로는 fetch 또는 AJAX 등을 사용하여 서버로 데이터를 전송합니다.
     if (email === 'test@example.com' && password === 'password123') {
-      window.location.href = `http://127.0.0.1:5500/index.html`
+      window.location.href = `index.html`
 
         // 여기서 리디렉션을 처리할 수 있습니다.
         // 예: window.location.href = '/dashboard';
